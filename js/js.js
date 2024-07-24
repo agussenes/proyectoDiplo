bfa3cc98e69dff4f74f9459c74a02eb58500f874
agussenes <agustinsenestrari00@gmail.com>
2024-07-23 23:18:58 -0300

    seccion-productos

--- js/js.js
+++ js/js.js
@@ -2,5 +2,5 @@
                 document.querySelector('.menu-icon').addEventListener('click', function () {
                 document.querySelector('nav-links').classList.toggle('show');
                 });
-
+                
             </script>
